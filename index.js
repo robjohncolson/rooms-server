a6c5520f6c9cc562d56716b4c5ab179074daf18c
robert colson <robjohncolson@protonmail.com>
2025-01-16 21:31:10 -0500

    git commit -m "Fix user management and connection handling"

--- index.js
+++ index.js
@@ -19,14 +19,33 @@ const io = new Server(httpServer, {
     credentials: true,
     transports: ['websocket', 'polling']
   },
-  allowEIO3: true
+  allowEIO3: true,
+  pingTimeout: 10000,
+  pingInterval: 5000
 });
 
 // Store connected users
 const users = new Map();
 
+// Clean up disconnected users periodically
+setInterval(() => {
+  for (const [socketId, user] of users.entries()) {
+    const socket = io.sockets.sockets.get(socketId);
+    if (!socket) {
+      users.delete(socketId);
+      io.emit('user-left', socketId);
+    }
+  }
+}, 10000);
+
 io.on('connection', (socket) => {
   console.log('New connection:', socket.id);
+  
+  // Clean up any existing user with this ID
+  if (users.has(socket.id)) {
+    users.delete(socket.id);
+    io.emit('user-left', socket.id);
+  }
   
   // Generate username and initial color with some default color
   const username = generateUsername();
